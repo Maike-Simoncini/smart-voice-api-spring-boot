f138bf0c11d4e99c34d4dca65b22d4c26e9c41ff
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2026-05-10 13:01:13 -0300

    Add TransactionRepository interface for JPA

---
+++ src/main/java/com/dio/smartvoice/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.dio.smartvoice.repository;
+
+import com.dio.smartvoice.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}

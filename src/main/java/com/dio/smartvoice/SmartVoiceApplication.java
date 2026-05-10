3091dce855f64f3a33d7f27d6dca540f099b1e04
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2026-05-10 12:58:56 -0300

    Initialize SmartVoiceApplication with Spring Boot

---
+++ src/main/java/com/dio/smartvoice/SmartVoiceApplication.java
@@ -0,0 +1,11 @@
+package com.dio.smartvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVoiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartVoiceApplication.class, args);
+    }
+}
